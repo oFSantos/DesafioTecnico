c787af57738852a6577f72af7d155fcf5f18fbd9
Santos <santosfelipes76@gmail.com>
2025-06-27 22:13:39 -0300

    correcao

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: 'http://localhost:5042/'
 });
 
-// Interceptor de REQUEST – já está certinho
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
@@ -13,18 +12,8 @@ api.interceptors.request.use(
     }
     return config;
   },
-  error => Promise.reject(error)
-);
-
-// 👇 Interceptor de RESPONSE – adiciona esse trecho abaixo
-api.interceptors.response.use(
-  response => response,
   error => {
-    const msg =
-      error.response?.data?.message || // se a API retorna { message: "..." }
-      error.response?.data ||          // se a API retorna string
-      'Erro inesperado';
-    return Promise.reject(new Error(msg));
+    return Promise.reject(error);
   }
 );
 
